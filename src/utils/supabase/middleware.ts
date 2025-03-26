e9da6bd0e6a3d369ce486cb308594d5ef4dde8a7
WonYus <lucy_oliv@outlook.com>
2025-03-26 15:40:33 +0700

    fix env

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -6,7 +6,7 @@ export async function updateSession(request: NextRequest) {
     request,
   });
 
-  const supabase = createServerClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!, {
+  const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
       getAll() {
         return request.cookies.getAll();
