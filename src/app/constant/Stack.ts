8e90eb8f4dd19a0802a32892c4b1851060cb3ac2
Imron Yusoh <imron.y@ktbg.tech>
2025-05-02 21:40:49 +0700

    fix model and add stacks

---
+++ src/app/constant/Stack.ts
@@ -0,0 +1,163 @@
+export interface Stack {
+    name: string;
+    description: string;
+    icon: string;
+}
+export const stacks: Stack[] = [
+    {
+        name: "Golang",
+        description:
+            "Golang is a programming language that is used for building high-performance applications. It is a statically typed language that is compiled into machine code. It is a great language for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original.svg",
+    },
+    {
+        name: "Node.js",
+        description:
+            "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. It is used for building scalable network applications. It is a great language for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    },
+    {
+        name: "Python",
+        description:
+            "Python is a programming language that is used for building high-performance applications. It is a statically typed language that is compiled into machine code. It is a great language for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    },
+    {
+        name: "TypeScript",
+        description:
+            "TypeScript is a programming language that is a superset of JavaScript. It is used for building large-scale applications. It is a great language for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    },
+    {
+        name: "React.js",
+        description:
+            "React.js is a JavaScript library for building user interfaces. It is used for building single-page applications. It is a great library for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    },
+    {
+        name: "Next.js",
+        description:
+            "Next.js is a React framework for building server-side rendered applications. It is used for building single-page applications. It is a great framework for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
+    },
+    {
+        name: "Angular",
+        description:
+            "Angular is a JavaScript framework for building user interfaces. It is used for building single-page applications. It is a great framework for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
+    },
+    {
+        name: "Tailwind CSS",
+        description:
+            "Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces. It is used for building responsive web applications. It is a great framework for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg",
+    },
+    {
+        name: "Material UI",
+        description:
+            "Material UI is a React UI framework that implements Google's Material Design. It is used for building responsive web applications. It is a great framework for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+    },
+    {
+        name: "Keycloack",
+        description:
+            "Keycloak is an open source identity and access management solution. It is used for managing identities and access to applications. It is a great tool for managing identities and access to applications.",
+        icon: "https://cdn.jsdelivr.net/gh/keycloak/keycloak-misc@main/logo/logo-key.svg",
+    },
+    {
+        name: "AWS",
+        description:
+            "AWS is a cloud computing platform that provides a wide range of services. It is used for building scalable applications. It is a great platform for building applications that need to be fast and efficient.",
+        icon: "/icons/AWS.512.png",
+    },
+    {
+        name: "Azure",
+        description:
+            "Azure is a cloud computing platform that provides a wide range of services. It is used for building scalable applications. It is a great platform for building applications that need to be fast and efficient.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg",
+    },
+    {
+        name: "Railway",
+        description:
+            "Railway is a platform for deploying and scaling serverless applications. It is used for deploying and scaling serverless applications. It is a great tool for deploying and scaling serverless applications.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/railway/railway-original.svg",
+    },
+    {
+        name: "Vercel",
+        description:
+            "Vercel is a cloud platform for static sites and Serverless Functions. It is used for deploying and scaling static sites and Serverless Functions. It is a great tool for deploying and scaling static sites and Serverless Functions.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg",
+    },
+    {
+        name: "Git",
+        description:
+            "Git is a distributed version control system. It is used for tracking changes in source code during software development. It is a great tool for managing source code.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    },
+    {
+        name: "GitHub",
+        description:
+            "GitHub is a web-based hosting service for version control using Git. It is used for storing and managing source code. It is a great tool for managing source code.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    },
+    {
+        name: "Docker",
+        description:
+            "Docker is a set of platform as a service products that use OS-level virtualization to deliver software in packages called containers. It is used for building and managing containers. It is a great tool for building and managing containers.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+    },
+    {
+        name: "Kubernetes",
+        description:
+            "Kubernetes is an open-source container orchestration system for automating deployment, scaling, and management of containerized applications. It is used for managing containers. It is a great tool for managing containers.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg",
+    },
+    {
+        name: "MongoDB",
+        description:
+            "MongoDB is a document-oriented database program that uses JSON-like documents with optional schemas. It is used for storing and managing data. It is a great database for storing and managing data.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+    },
+    {
+        name: "MySQL",
+        description:
+            "MySQL is a relational database management system (RDBMS) that runs as a server providing multi-user access to a number of databases. It is used for storing and managing data. It is a great database for storing and managing data.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+    },
+    {
+        name: "PostgreSQL",
+        description:
+            "PostgreSQL is a powerful, open source object-relational database system. It is used for storing and managing data. It is a great database for storing and managing data.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+    },
+    {
+        name: "Redis",
+        description:
+            "Redis is an open source (BSD licensed), in-memory data structure store, used as a database, cache and message broker. It is used for storing and managing data. It is a great database for storing and managing data.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original.svg",
+    },
+    {
+        name: "Grafana",
+        description:
+            "Grafana is an open source platform for monitoring and observability. It is used for monitoring and observability. It is a great tool for monitoring and observability.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/grafana/grafana-original.svg",
+    },
+    {
+        name: "Prometheus",
+        description:
+            "Prometheus is an open source monitoring system with a dimensional data model, flexible query language, efficient time series database and modern alerting approach. It is used for monitoring and observability. It is a great tool for monitoring and observability.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/prometheus/prometheus-original.svg",
+    },
+    {
+        name: "Airflow",
+        description:
+            "Airflow is a platform to programmatically author, schedule and monitor workflows. It is used for scheduling and monitoring workflows. It is a great tool for scheduling and monitoring workflows.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apacheairflow/apacheairflow-original.svg",
+    },
+    {
+        name: "Jenkins",
+        description:
+            "Jenkins is an open source automation server. It is used for building and testing software projects continuously. It is a great tool for building and testing software projects continuously.",
+        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg",
+    },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,73 @@
+import Image from "next/image";
 import { CarreerPath, carreerPath } from "./constant/CareerPath";
 import { iconLinks } from "./constant/IconLink";
+import { Stack, stacks } from "./constant/Stack";
 
 export default function Home() {
-  const MapCareerPath = ({ career, index }: { career: CarreerPath; index: number }) => {
-    return (
-      <div className="bg-tertiary p-5 rounded-2xl w-full" key={index}>
-        <div className="flex flex-col gap-1">
-          <div className="flex gap-1 flex-col">
-            <h3 className="font-bold">{career.organization}</h3>
-            <p className="text-sm">{career.date}</p>
-          </div>
-          <p className="text-sm">{career.location}</p>
-        </div>
-        <h3 className="font-bold">{career.title}</h3>
-        <p className="text-sm">{career.description}</p>
-      </div>
-    );
-  };
+    const MapCareerPath = ({ carreerPath }: { carreerPath: CarreerPath[] }) => {
+        const careerCard = (career: CarreerPath, index: number) => {
+            return (
+                <div className="bg-tertiary p-5 rounded-2xl w-full" key={index}>
+                    <div className="flex flex-col gap-1">
+                        <div className="flex gap-1 flex-col">
+                            <h3 className="font-bold">{career.organization}</h3>
+                            <p className="text-sm">{career.date}</p>
+                        </div>
+                        <p className="text-sm">{career.location}</p>
+                    </div>
+                    <h3 className="font-bold">{career.title}</h3>
+                    <p className="text-sm">{career.description}</p>
+                </div>
+            );
+        };
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <section>IMRON YUSOH</section>
-        <div className="flex gap-4 items-center sm:items-start">
-          {iconLinks.map(({ href, icon }) => (
-            <a key={href} href={href} target="_blank" rel="noopener noreferrer" className="text-2xl">
-              {icon}
-            </a>
-          ))}
-        </div>{" "}
-        <div>
-          <h1>Career Path</h1>
-          {carreerPath.map((career, index) => MapCareerPath({ career, index }))}
+        return (
+            <div>
+                <h1>Career Path</h1>
+                <div className="flex flex-col gap-4">
+                    {carreerPath.map((career, index) => careerCard(career, index))}
+                </div>
+            </div>
+        );
+    };
+
+    const MapStack = ({ stacks }: { stacks: Stack[] }) => {
+        const stackCard = (s: Stack, i: number) => {
+            return (
+                <div className="bg-tertiary p-5 rounded-2xl w-[230px] h-[250px]" key={i}>
+                    <div className="flex-row gap-2 items-center text-center" key={i}>
+                        <div className="flex flex-row gap-1 justify-center items-center">
+                            <Image src={s.icon} alt={s.name} width={40} height={40} />
+                            <h3 className="font-bold">{s.name}</h3>
+                        </div>
+                        <p className="text-sm">{s.description}</p>
+                    </div>
+                </div>
+            );
+        };
+
+        return (
+            <div>
+                <p>Stack</p>{" "}
+                <div className="flex flex-wrap gap-4">{stacks.map((stack, index) => stackCard(stack, index))}</div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+                <section>IMRON YUSOH</section>
+                <div className="flex gap-4 items-center sm:items-start">
+                    {iconLinks.map(({ href, icon }) => (
+                        <a key={href} href={href} target="_blank" rel="noopener noreferrer" className="text-2xl">
+                            {icon}
+                        </a>
+                    ))}
+                </div>{" "}
+                {MapCareerPath({ carreerPath: carreerPath })}
+                {MapStack({ stacks: stacks })}
+            </main>
         </div>
-        <div>
-          <p>Stack</p>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
