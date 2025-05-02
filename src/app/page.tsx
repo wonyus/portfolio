4c7943805970c967af07774cf3835e88c03feb46
WonYus <lucy_oliv@outlook.com>
2025-05-02 21:43:04 +0700

    add bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import { CarreerPath, carreerPath } from "./constant/CareerPath";
 import { iconLinks } from "./constant/IconLink";
+import GitHubCalendar from "react-github-calendar";
 
 export default function Home() {
   const MapCareerPath = ({ career, index }: { career: CarreerPath; index: number }) => {
@@ -19,24 +21,39 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <section>IMRON YUSOH</section>
-        <div className="flex gap-4 items-center sm:items-start">
-          {iconLinks.map(({ href, icon }) => (
-            <a key={href} href={href} target="_blank" rel="noopener noreferrer" className="text-2xl">
-              {icon}
-            </a>
-          ))}
-        </div>{" "}
-        <div>
-          <h1>Career Path</h1>
-          {carreerPath.map((career, index) => MapCareerPath({ career, index }))}
-        </div>
-        <div>
-          <p>Stack</p>
-        </div>
-      </main>
+    <div className="relative overflow-hidden ">
+      <div className="relative grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start text-white/90">
+          <section>IMRON YUSOH</section>
+          <div className="flex gap-4 items-center sm:items-start">
+            {iconLinks.map(({ href, icon }) => (
+              <a key={href} href={href} target="_blank" rel="noopener noreferrer" className="text-2xl">
+                {icon}
+              </a>
+            ))}
+          </div>{" "}
+          <div>
+            <h1>Career Path</h1>
+            {carreerPath.map((career, index) => MapCareerPath({ career, index }))}
+          </div>
+          <div>
+            <p>Stack</p>
+          </div>
+          <div className="container">
+            <p>Github Contribution</p>
+            <div className="flex gap-4">
+              <div className="flex flex-col gap-4">
+                <Link className="text-blue-500 underline row-start-2 pt-4" href="https://github.com/wonyus" target="_blank" rel="noopener noreferrer">
+                  GithubLink
+                </Link>
+              </div>
+              <div className="w-full flex justify-center">
+                <GitHubCalendar username="wonyus" />
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
