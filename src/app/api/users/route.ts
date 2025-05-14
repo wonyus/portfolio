57584e79740ee127dba5ce6809b193c01141a5d5
Imron Yusoh <imron.y@ktbg.tech>
2025-05-15 01:40:19 +0700

    hot fix prisma

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,28 +1,28 @@
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  return new Response("Method not allowed", { status: 405 });
+    return new Response("Method not allowed", { status: 405 });
 }
 
 export async function POST(req: NextRequest) {
-  //create user
-  const body = await req.json();
+    //create user
+    const body = await req.json();
 
-  const user = await prisma.user.create({
-    data: {
-      name: body.name,
-      email: body.email,
-    },
-  });
+    const user = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email,
+        },
+    });
 
-  return new Response(JSON.stringify(user), { status: 201 });
+    return new Response(JSON.stringify(user), { status: 201 });
 }
 
 export async function PUT(req: NextRequest) {
-  return new Response("Method not allowed", { status: 405 });
+    return new Response("Method not allowed", { status: 405 });
 }
 
 export async function DELETE(req: NextRequest) {
-  return new Response("Method not allowed", { status: 405 });
+    return new Response("Method not allowed", { status: 405 });
 }
--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,26 +1,26 @@
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 
 export default async function Post({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  const project = await prisma.project.findUnique({
-    where: { id: id },
-    include: {
-      user: true,
-    },
-  });
+    const { id } = await params;
+    const project = await prisma.project.findUnique({
+        where: { id: id },
+        include: {
+            user: true,
+        },
+    });
 
-  if (!project) {
-    notFound();
-  }
+    if (!project) {
+        notFound();
+    }
 
-  return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <article className="max-w-2xl space-y-4 font-[family-name:var(--font-geist-sans)]">
-        <h1 className="text-4xl font-bold mb-8 text-[#333333]">{project.title}</h1>
-        <p className="text-gray-600 text-center">by {project.user?.name ?? "Unknown"}</p>
-        <div className="prose prose-gray mt-8">{project.description || "No content available."}</div>
-      </article>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+            <article className="max-w-2xl space-y-4 font-[family-name:var(--font-geist-sans)]">
+                <h1 className="text-4xl font-bold mb-8 text-[#333333]">{project.title}</h1>
+                <p className="text-gray-600 text-center">by {project.user?.name ?? "Unknown"}</p>
+                <div className="prose prose-gray mt-8">{project.description || "No content available."}</div>
+            </article>
+        </div>
+    );
 }
